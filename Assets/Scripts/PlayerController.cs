106a131e925e13df1e00c3f08ae3d9c6f5c18699
HandyCrabs <ntbanks924@gmail.com>
2022-12-10 16:27:54 -0500

    Folders and basic player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    private RigidBody2D rb;
+
+    void start(){
+
+        rb = GetComponent<Rigi
+    }
+
+}
