f8777a424c89fab00611b501dd05a9d0d5ddb522
HandyCrabs <ntbanks924@gmail.com>
2022-12-14 16:01:08 -0500

    Smooth x/y axis from input
    Remove raw axis input from player mvt scripts.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         moveVelocity = moveInput.normalized * speed;
         
     }
