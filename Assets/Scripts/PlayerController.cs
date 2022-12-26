d645efdaea2eef481271cb1766fad01b974a1d2a
HandyCrabs <ntbanks924@gmail.com>
2022-12-26 11:12:32 -0500

    movement update
    player movement script
    new assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,14 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
+    private Animator anim;
+    float inputHorizontal;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,7 +22,24 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * speed;
-        
+
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+    
+        if (moveInput.x == 0 && moveInput.y == 0) {
+            anim.SetBool("isRunning",false);
+        }
+        else {  
+            anim.SetBool("isRunning",true);
+        }
+
+        //movement to the right will cause 
+        if (inputHorizontal > 0) {
+            transform.localScale = new Vector3 (1,1);                    
+        }
+        else {
+            transform.localScale = new Vector3 (-1,-1);
+        }
+
     }
 
     void FixedUpdate(){
