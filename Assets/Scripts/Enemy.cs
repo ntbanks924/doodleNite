d2d520877d6594e8866122d26a0dd00d69e2c71b
HandyCrabs <ntbanks924@users.noreply.github.com>
2022-12-30 10:49:28 -0500

    New assets sets added to project

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,15 +6,28 @@ public class Enemy : MonoBehaviour
 {
 
     public int health;
+    public float speed;
+    public Transform target;
+    public float minDistance;
+
     public void TakeDamage(int damage) {
         health -= damage;
     }
 
-    private void Update(){
+    private void Update()
+    {
         if (health <= 0) {
             Destroy(gameObject);
         }
-    }
 
+        ///Stop moving towards character once min distance is reached
+        if (Vector2.Distance(transform.position, target.position) > minDistance) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        } else
+        {
+            /// Add attack code here for dealing damage to player.
+        }
+    }
 
 }
---
+++ Assets/Scripts/cursor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
